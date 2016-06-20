70b6a16911a0c5203fa628d51fd640aa60381f17
heiheiha <1137029429@qq.com>
2016-06-20 09:52:10 +0800

    add1...100
    从1累加到100，先设置俩个变量sum ，i。设置变量的方法是var sum＝0 还可以设置变量的类型 var sum：int＝100
    然后用一个for循环累加
    for i in i…100｛
         sum＝sum＋i
    ｝
    最后输出sum就可以了

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  add1...100
+//
+//  Created by  黄磊 on 16/6/20.
+//  Copyright © 2016年  黄磊. All rights reserved.
+//
+
+import Foundation
+var sum=0
+var i=1
+for i in 1...100{
+    sum=sum+i
+}
+print(sum)
+
